ef193e2d62155c6cff0b257ef65862a914b31786
chengyanlong <chengyanlong@taiyouxi.cn>
2024-01-05 17:19:20 +0800

    升级 xray 版本

--- Formula/xray2.rb
+++ Formula/xray2.rb
@@ -2,7 +2,7 @@ class Xray2 < Formula
   desc "Platform for building proxies to bypass network restrictions"
   homepage "https://xtls.github.io/"
   url "https://github.com/XTLS/Xray-core/releases/download/v1.8.6/Xray-macos-64.zip"
-  sha256 "251c9455fd2793072d534e180eae60844d3ec05566c22009e7a7b8abf93371fc"
+  sha256 "54616fe61381dad92cfa30030dda3211b3c2e86eb496b18a0462451e29728b5e"
   license all_of: ["MPL-2.0", "CC-BY-SA-4.0"]
   version "v1.8.6"
 
