48eb115432008616a0aa5bd7e3a9870ffd2e5dbf
chengyanlong <chengyanlong@taiyouxi.cn>
2024-01-05 17:03:29 +0800

    升级 naiveproxy 版本

--- Formula/naiveproxy4.rb
+++ Formula/naiveproxy4.rb
@@ -3,13 +3,13 @@ class Naiveproxy4 < Formula
   homepage "https://github.com/klzgrad/naiveproxy"
   # download binary from upstream directly
   if Hardware::CPU.intel?
-    url "https://github.com/klzgrad/naiveproxy/releases/download/v118.0.5993.65-1/naiveproxy-v118.0.5993.65-1-mac-x64.tar.xz"
+    url "https://github.com/klzgrad/naiveproxy/releases/download/v120.0.6099.43-1/naiveproxy-v120.0.6099.43-1-mac-x64.tar.xz"
     sha256 "595d488e4b55aaaddd815a9f38fdc9d6139a2cfa12327928da5c2820793ba611"
   else
-    url "https://github.com/klzgrad/naiveproxy/releases/download/v118.0.5993.65-1/naiveproxy-v118.0.5993.65-1-mac-arm64.tar.xz"
+    url "https://github.com/klzgrad/naiveproxy/releases/download/v120.0.6099.43-1/naiveproxy-v120.0.6099.43-1-mac-arm64.tar.xz"
     sha256 "3c12ce7797f420c00b813f0059107c8f9b70951429fcbbd9a9a864bcd900bbb7"
   end
-  version "v118.0.5993.65-1"
+  version "v120.0.6099.43-1"
   license "BSD 3-Clause"
 
   def install
